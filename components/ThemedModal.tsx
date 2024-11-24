9aa4fda7ab3df71bff504b356590297becfe0bad
Pablo Pirata <pablo@studocu.com>
2024-11-24 21:42:34 +0100

    new themed modal

--- components/ThemedModal.tsx
+++ components/ThemedModal.tsx
@@ -1,15 +1,15 @@
-import { addTransparency, CustomThemeColorsType, CustomThemeType } from '@/utils/colors';
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Animated, Modal, Pressable, StyleSheet, View, ViewStyle } from 'react-native';
-import { Button, Text, useTheme } from 'react-native-paper';
+import { CustomThemeColorsType, CustomThemeType } from '@/utils/colors';
+import React, { memo, useCallback, useState } from 'react';
+import { StyleSheet, TouchableWithoutFeedback, View, ViewStyle } from 'react-native';
+import { Button, Modal, Portal, Text, useTheme } from 'react-native-paper';
 
 interface ThemedModalProps {
     cancelText?: string;
     children?: React.ReactNode;
     closeOnTouchOutside?: boolean;
     confirmText?: string;
-    onClose: (() => Promise<void>) | (() => void);
-    onConfirm?: (() => Promise<void>) | (() => void);
+    onClose: () => void;
+    onConfirm?: () => (() => void) | Promise<void>;
     style?: ViewStyle;
     title?: string;
     visible: boolean;
@@ -26,18 +26,9 @@ const ThemedModal = ({
     title,
     visible,
 }: ThemedModalProps) => {
-    const [isVisible, setIsVisible] = useState(visible);
     const [isLoading, setIsLoading] = useState(false);
-    const fadeAnim = useRef(new Animated.Value(0)).current;
-    const slideAnim = useRef(new Animated.Value(300)).current;
     const { colors, dark } = useTheme<CustomThemeType>();
     const styles = makeStyles(colors, dark);
-
-    const handleOnClose = useCallback(async () => {
-        setIsLoading(true);
-        await onClose();
-        setIsLoading(false);
-    }, [onClose]);
 
     const handleOnConfirm = useCallback(async () => {
         setIsLoading(true);
@@ -45,84 +36,66 @@ const ThemedModal = ({
         setIsLoading(false);
     }, [onConfirm]);
 
-    useEffect(() => {
-        if (visible) {
-            setIsVisible(true);
-            Animated.parallel([
-                Animated.timing(fadeAnim, {
-                    duration: 300,
-                    toValue: 1,
-                    useNativeDriver: true,
-                }),
-                Animated.timing(slideAnim, {
-                    duration: 300,
-                    toValue: 0,
-                    useNativeDriver: true,
-                }),
-            ]).start();
-        } else {
-            Animated.parallel([
-                Animated.timing(fadeAnim, {
-                    duration: 300,
-                    toValue: 0,
-                    useNativeDriver: true,
-                }),
-                Animated.timing(slideAnim, {
-                    duration: 300,
-                    toValue: 300,
-                    useNativeDriver: true,
-                }),
-            ]).start(() => setIsVisible(false));
-        }
-    }, [fadeAnim, slideAnim, visible]);
+    const handleOnClose = useCallback(() => {
+        setIsLoading(false);
+        onClose();
+    }, [onClose]);
 
-    if (!isVisible) {
-        return null;
-    }
+    const handleDismissOnOverlayPress = useCallback(() => {
+        if (closeOnTouchOutside) {
+            handleOnClose();
+        }
+    }, [closeOnTouchOutside, handleOnClose]);
 
     return (
-        <Modal
-            animationType="none"
-            onRequestClose={handleOnClose}
-            transparent={true}
-            visible={isVisible}
-        >
-            <Pressable
-                onPress={closeOnTouchOutside ? handleOnClose : undefined}
-                style={styles.modalOverlay}
-            >
-                <Animated.View style={[styles.modalContent, style, { opacity: fadeAnim, transform: [{ translateY: slideAnim }] }]}>
-                    <Pressable style={styles.innerPressable}>
-                        {title && (
-                            <Text style={styles.modalMessage}>{title}</Text>
-                        )}
-                        {children}
-                        <View style={styles.buttonContainer}>
-                            {cancelText && (
-                                <Button
-                                    disabled={isLoading}
-                                    mode="outlined"
-                                    onPress={handleOnClose}
-                                    style={[styles.button, !onConfirm ? styles.wideButton : {}]}
-                                >
-                                    {cancelText}
-                                </Button>
-                            )}
-                            {(confirmText && onConfirm) && (
-                                <Button
-                                    disabled={isLoading}
-                                    mode="contained"
-                                    onPress={handleOnConfirm}
-                                    style={styles.button}
-                                >
-                                    {confirmText}
-                                </Button>
-                            )}
-                        </View>
-                    </Pressable>
-                </Animated.View>
-            </Pressable>
-        </Modal>
+        <Portal>
+            {visible && (
+                <TouchableWithoutFeedback
+                    accessible={false} // Prevent the overlay from interfering with accessibility
+                    onPress={handleDismissOnOverlayPress}
+                >
+                    <View style={styles.overlay}>
+                        <Modal
+                            contentContainerStyle={[styles.modalContent, style]}
+                            dismissable={false} // We handle dismissal ourselves
+                            visible={visible}
+                        >
+                            <View style={styles.innerContent}>
+                                {title && (
+                                    <Text style={styles.modalMessage}>{title}</Text>
+                                )}
+                                {children}
+                                <View style={styles.buttonContainer}>
+                                    {cancelText && (
+                                        <Button
+                                            disabled={isLoading}
+                                            mode="outlined"
+                                            onPress={handleOnClose}
+                                            style={[
+                                                styles.button,
+                                                !onConfirm ? styles.wideButton : {},
+                                            ]}
+                                        >
+                                            {cancelText}
+                                        </Button>
+                                    )}
+                                    {confirmText && onConfirm && (
+                                        <Button
+                                            disabled={isLoading}
+                                            mode="contained"
+                                            onPress={handleOnConfirm}
+                                            style={styles.button}
+                                        >
+                                            {confirmText}
+                                        </Button>
+                                    )}
+                                </View>
+                            </View>
+                        </Modal>
+                    </View>
+                </TouchableWithoutFeedback>
+            )}
+        </Portal>
     );
 };
 
@@ -135,11 +108,12 @@ const makeStyles = (colors: CustomThemeColorsType, dark: boolean) => StyleSheet.
         justifyContent: 'space-between',
         width: '100%',
     },
-    innerPressable: {
+    innerContent: {
         width: '100%',
     },
     modalContent: {
         alignItems: 'center',
+        alignSelf: 'center', // Center the modal horizontally
         backgroundColor: colors.background,
         borderColor: colors.shadow,
         borderRadius: 8,
@@ -153,9 +127,9 @@ const makeStyles = (colors: CustomThemeColorsType, dark: boolean) => StyleSheet.
         marginBottom: 20,
         textAlign: 'center',
     },
-    modalOverlay: {
+    overlay: {
         alignItems: 'center',
-        backgroundColor: addTransparency(colors.background, 0.5),
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent overlay
         flex: 1,
         justifyContent: 'center',
     },
@@ -165,4 +139,4 @@ const makeStyles = (colors: CustomThemeColorsType, dark: boolean) => StyleSheet.
     },
 });
 
-export default ThemedModal;
+export default memo(ThemedModal);
