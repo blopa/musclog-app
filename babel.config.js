54f4ee847d2f4593129d55cc5e424542fb9d0708
Pablo Pirata <pablo@studocu.com>
2026-01-05 14:16:03 +0100

    Initial commit
    Generated by create-expo-stack 2.20.1

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+  api.cache(true);
+  let plugins = [];
+
+  plugins.push('react-native-worklets/plugin');
+
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+    plugins,
+  };
+};
