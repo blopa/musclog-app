f8aa5b6fd03abe71015413c77a45ed9e6d86a9df
Pablo Pirata <pablo@studocu.com>
2026-01-11 12:38:53 +0100

    update package

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
-      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
       // ONLY use the reanimated plugin.
       // In the new versions, it automatically handles the worklets logic.
       'react-native-reanimated/plugin',
