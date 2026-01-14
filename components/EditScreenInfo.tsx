8a2e0e06d7feb039336be22d4b21a09f3c9437af
Pablo Pirata <pablo@studocu.com>
2026-01-15 00:19:20 +0100

    add GenericCard

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,9 +1,10 @@
 import { Text, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
 export const EditScreenInfo = ({ path }: { path: string }) => {
-  const title = 'Open up the code for this screen:';
-  const description =
-    'Change any of the text, save the file, and your app will automatically update.';
+  const { t } = useTranslation();
+  const title = t('editScreenInfo.title');
+  const description = t('editScreenInfo.description');
 
   return (
     <View>
