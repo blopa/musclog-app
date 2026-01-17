a3a68537de2b52fb2b1ac92b221f4884301b7749
Pablo Pirata <pablo@studocu.com>
2026-01-17 17:47:50 +0100

    add babel

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,8 @@ module.exports = function (api) {
     plugins: [
       ['@babel/plugin-proposal-decorators', { legacy: true }],
       ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
       // ONLY use the reanimated plugin.
       // In the new versions, it automatically handles the worklets logic.
       'react-native-reanimated/plugin',
