3586684b98a1d3c30bb30f3f96901da0c11a56f5
Pablo Pirata <pablo@studocu.com>
2026-02-04 19:52:56 +0100

    aaa

--- components/modals/PortionSizesPickerModal.tsx
+++ components/modals/PortionSizesPickerModal.tsx
@@ -89,6 +89,7 @@ export function PortionSizesPickerModal({
     if (!searchQuery.trim()) {
       return selectorOptions;
     }
+
     const query = searchQuery.toLowerCase();
     return selectorOptions.filter(
       (option) =>
