0270b931dd4cb9c60484c4d1ffdd62364c13b7c4
Pablo Pirata <pablo@studocu.com>
2026-02-21 23:05:12 +0100

    lint

--- hooks/useWorkoutForm.ts
+++ hooks/useWorkoutForm.ts
@@ -189,6 +189,7 @@ export function useWorkoutForm({ templateId }: UseWorkoutFormParams = {}) {
   }, [
     workoutTitle,
     description,
+    volumeCalc,
     exercises,
     exerciseMetadata,
     selectedDays,
