02105a54832b45a146f1ca3678697affa9624270
Pablo Pirata <pablo@studocu.com>
2026-02-23 21:02:51 +0100

    rollback

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,28 @@
 module.exports = function (api) {
+  // Check caller before configuring cache
+  const isWebFromCaller = api.caller(
+    (caller) =>
+      caller?.platform === 'web' || caller?.name === 'metro-web' || caller?.name === 'webpack'
+  );
+
   api.cache(true);
+
+  const isWeb =
+    process.env.EXPO_PLATFORM === 'web' ||
+    process.env.BABEL_ENV === 'web' ||
+    process.env.WEBPACK_DEV_SERVER === 'true' ||
+    isWebFromCaller;
 
   const plugins = [['@babel/plugin-proposal-decorators', { legacy: true }]];
 
-  plugins.push(
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }],
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
-  );
+  // Always use loose: true for web to handle definite assignment with decorators
+  if (isWeb) {
+    plugins.push(
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+    );
+  }
 
   // ONLY use the reanimated plugin.
   // In the new versions, it automatically handles the worklets logic.
