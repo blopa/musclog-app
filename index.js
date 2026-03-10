9723b4864ae19c33a2550590cde6e780b6a36ed4
Pablo Pirata <pablo@studocu.com>
2026-03-10 22:11:32 +0100

    fix widget

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'expo-router/entry';
 
+import { registerWidgetTaskHandler } from 'react-native-android-widget';
+
 import { widgetTaskHandler } from './widgets/widget-task-handler';
 
-// Ensure widgetTaskHandler is initialized
-console.log('Widget Task Handler registered:', !!widgetTaskHandler);
+registerWidgetTaskHandler(widgetTaskHandler);
--- widgets/widget-task-handler.ts
+++ widgets/widget-task-handler.ts
@@ -1,11 +1,11 @@
-import { registerWidgetTaskHandler } from 'react-native-android-widget';
+import type { WidgetTaskHandlerProps } from 'react-native-android-widget';
 
 import { SmartCameraWidget } from './SmartCameraWidget';
 
-export const widgetTaskHandler = registerWidgetTaskHandler((props) => {
-  const { widgetName, widgetAction } = props;
+export async function widgetTaskHandler(props: WidgetTaskHandlerProps) {
+  const { widgetInfo, widgetAction } = props;
 
-  switch (widgetName) {
+  switch (widgetInfo.widgetName) {
     case 'SmartCamera':
       if (widgetAction === 'WIDGET_ADDED' || widgetAction === 'WIDGET_UPDATE') {
         props.renderWidget(SmartCameraWidget());
@@ -15,4 +15,4 @@ export const widgetTaskHandler = registerWidgetTaskHandler((props) => {
     default:
       break;
   }
-});
+}
