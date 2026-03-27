c60c6f65f7621750d34db9e21db5bae987a7ee3a
Pablo Pirata <pablo@studocu.com>
2026-03-27 22:42:35 +0100

    add todo

--- database/schema.ts
+++ database/schema.ts
@@ -13,6 +13,7 @@ export const schema = appSchema({
         { name: 'muscle_group', type: 'string', isIndexed: true },
         { name: 'equipment_type', type: 'string', isIndexed: true }, // Dumbbell, Barbell, Bodyweight
         { name: 'mechanic_type', type: 'string' }, // 'compound' or 'isolation'
+        // { name: 'source', type: 'string' }, // 'app' or 'user' // TODO: add this and update models, migrations, services and hooks, and set the value to 'app' when creating from the dev.ts or prod.ts, and 'user' when creating from the user
         { name: 'load_multiplier', type: 'number' }, // Load multiplier for volume calculations
         { name: 'created_at', type: 'number' },
         { name: 'updated_at', type: 'number' },
