aab44c79614de5ddce941e9a5b4a17301a34ef59
Pablo Pirata <pablo@studocu.com>
2026-04-12 19:19:28 +0200

    add constant for macros

---
+++ constants/nutrition.ts
@@ -0,0 +1,6 @@
+// TODO: use these values
+export const CALORIES_FOR_FAT = 9;
+export const CALORIES_FOR_PROTEIN = 4;
+export const CALORIES_FOR_CARBS = 4;
+export const CALORIES_FOR_FIBER = 2;
+export const CALORIES_FOR_ALCOHOL = 7;
