0c52f0001af4adab9a53fc20be7bc144505e26c5
Pablo Pirata <pablo@studocu.com>
2026-04-27 18:40:40 +0200

    better ui

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,6 @@
-import { Redirect, useRouter } from 'expo-router';
-import { useEffect } from 'react';
+import { Redirect } from 'expo-router';
 import { Platform } from 'react-native';
 
 export default function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    // If platform is web, redirect user to home.tsx
-    if (Platform.OS === 'web' && !__DEV__) {
-      router.replace('/home');
-    }
-  }, [router]);
-
-  // Show loading state while redirecting on web platforms
-  if (Platform.OS === 'web') {
-    return null;
-  }
-
-  // TODO: cant we just do href={Platform.OS === 'web' && !__DEV__ ? '/home' : '/app'}?
-  return <Redirect href="/app" />;
+  return <Redirect href={Platform.OS === 'web' && !__DEV__ ? '/home' : '/app'} />;
 }
