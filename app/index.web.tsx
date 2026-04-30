d3e9bb92933dc6b721a51c5d86d0fbbaa9678629
Pablo Pirata <pablo@studocu.com>
2026-04-30 18:47:54 +0200

    add stuff

--- app/index.web.tsx
+++ app/index.web.tsx
@@ -1,5 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  return <Redirect href={!__DEV__ ? '/home' : '/app'} />;
+  return <Redirect href={__DEV__ ? '/app' : '/home'} />;
 }
