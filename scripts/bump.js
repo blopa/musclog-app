7e92fb93d266ccb5ee6bba1ccc5cdd47fb613666
Pablo Pirata <pablo@studocu.com>
2026-05-28 23:14:07 +0200

    bump app version

--- scripts/bump.js
+++ scripts/bump.js
@@ -7,18 +7,45 @@ function bumpBuildNumber(buildNumber) {
   return (parseInt(buildNumber, 10) + 1).toString();
 }
 
-function bumpVersion(version) {
-  const versionParts = version.split('.').map(Number);
-  versionParts[2] += 1;
+function parseBumpType(argv) {
+  const allowedFlags = new Set(['--major', '--minor']);
+  const unknownFlags = argv.filter((arg) => !allowedFlags.has(arg));
 
-  if (versionParts[2] >= 10) {
-    versionParts[2] = 0;
-    versionParts[1] += 1;
+  if (unknownFlags.length > 0) {
+    throw new Error(`Unknown flag(s): ${unknownFlags.join(', ')}`);
   }
 
-  if (versionParts[1] >= 10) {
-    versionParts[1] = 0;
+  const hasMajor = argv.includes('--major');
+  const hasMinor = argv.includes('--minor');
+
+  if (hasMajor && hasMinor) {
+    throw new Error('Use only one of --major or --minor at a time');
+  }
+
+  if (hasMajor) {
+    return 'major';
+  }
+
+  if (hasMinor) {
+    return 'minor';
+  }
+
+  return 'patch';
+}
+
+function bumpVersion(version, bumpType) {
+  const versionParts = version.split('.').map((part) => Number.parseInt(part, 10));
+
+  if (versionParts.length !== 3 || versionParts.some(Number.isNaN)) {
+    throw new Error(`Invalid version format: ${version}`);
+  }
+
+  if (bumpType === 'major') {
     versionParts[0] += 1;
+  } else if (bumpType === 'minor') {
+    versionParts[1] += 1;
+  } else {
+    versionParts[2] += 1;
   }
 
   return versionParts.join('.');
@@ -28,10 +55,10 @@ function bumpVersionCode(versionCode) {
   return parseInt(versionCode, 10) + 1;
 }
 
-function updateVersion(filePath) {
+function updateVersion(filePath, bumpType) {
   const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
   const oldVersion = data.version || (data.expo && data.expo.version);
-  const newVersion = bumpVersion(oldVersion);
+  const newVersion = bumpVersion(oldVersion, bumpType);
 
   if (data.version) {
     data.version = newVersion;
@@ -59,19 +86,20 @@ function updateVersion(filePath) {
 }
 
 const doTask = () => {
+  const bumpType = parseBumpType(process.argv.slice(2));
   const packageJsonPath = path.join(DIRNAME, 'package.json');
   const packageLockJsonPath = path.join(DIRNAME, 'package-lock.json');
   const appJsonPath = path.join(DIRNAME, 'app.json');
 
-  updateVersion(packageJsonPath);
+  updateVersion(packageJsonPath, bumpType);
 
   if (fs.existsSync(packageLockJsonPath)) {
-    updateVersion(packageLockJsonPath);
+    updateVersion(packageLockJsonPath, bumpType);
   } else {
     console.log(`${path.relative(DIRNAME, packageLockJsonPath)} not found, skipping`);
   }
 
-  updateVersion(appJsonPath);
+  updateVersion(appJsonPath, bumpType);
 };
 
 doTask();
