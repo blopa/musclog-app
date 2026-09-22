a52a817e25a279974b43de073a0b6c4a7ba96354
Pablo Pirata <montenegro.pablo@outlook.com>
2026-09-22 17:05:14 +0200

    lint

--- components/modals/MealEstimationModal.tsx
+++ components/modals/MealEstimationModal.tsx
@@ -25,9 +25,11 @@ const inferMealTypeFromTime = (date: Date): MealType => {
   if (hour >= 5 && hour < 11) {
     return 'breakfast';
   }
+
   if (hour >= 11 && hour < 15) {
     return 'lunch';
   }
+
   if (hour >= 15 && hour < 22) {
     return 'dinner';
   }
